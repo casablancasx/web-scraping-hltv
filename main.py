3c4bc24ae579e85b56368a42db88988a98854beb
Danilo Chaves <danchavesds@gmail.com>
2025-04-24 20:49:46 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from app.controller.hltv_info_controller import router as hltv_router
+
+app = FastAPI()
+app.include_router(hltv_router)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
